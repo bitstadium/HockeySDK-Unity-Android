90bd470d62071e90ad30740d1bf1d62025b40eb7
Christoph Wendt <christoph.wendt@me.com>
2014-06-20 13:17:36 +0200

    Update plugin sources (enable custom server urls)

--- HockeyAppUnity/src/net/hockeyapp/unity/HockeyUnityPlugin.java
+++ HockeyAppUnity/src/net/hockeyapp/unity/HockeyUnityPlugin.java
@@ -48,13 +48,47 @@ public class HockeyUnityPlugin {
 	}
 
 	@TargetApi(Build.VERSION_CODES.GINGERBREAD)
+	public static void startHockeyAppManager(final Activity currentActivity,
+			final String serverURL, final String appID,
+			final boolean updateManagerEnabled, final boolean autoSendEnabled) {
+
+		currentActivity.runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				if (updateManagerEnabled) {
+					UpdateManager.register(currentActivity, serverURL, appID, null);
+				}
+				CrashManager.register(currentActivity, serverURL, appID,
+						new CrashManagerListener() {
+							public boolean shouldAutoUploadCrashes() {
+								return autoSendEnabled;
+							}
+						});
+			}
+		});
+	}
+
+	@TargetApi(Build.VERSION_CODES.GINGERBREAD)
 	public static void startFeedbackForm(final String appID,
 			final Activity currentActivity) {
 
 		currentActivity.runOnUiThread(new Runnable() {
 			@Override
 			public void run() {
-				FeedbackManager.register(currentActivity, appID, null);
+				FeedbackManager.register(currentActivity, appID);
+				FeedbackManager.showFeedbackActivity(currentActivity);
+			}
+		});
+	}
+
+	@TargetApi(Build.VERSION_CODES.GINGERBREAD)
+	public static void startFeedbackForm(final Activity currentActivity,
+			final String serverURL, final String appID) {
+
+		currentActivity.runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				FeedbackManager.register(currentActivity, serverURL, appID, null);
 				FeedbackManager.showFeedbackActivity(currentActivity);
 			}
 		});
