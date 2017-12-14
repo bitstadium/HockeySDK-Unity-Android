48668863f07d2844ef30be362310c38e8b4635e0
Ivan Matkov <v-ivmatk@microsoft.com>
2017-12-14 17:22:24 +0300

    Copy script to example project

--- ExampleGame/Assets/HockeyAppUnityAndroid/HockeyAppUnity-Scripts/HockeyAppAndroid.cs
+++ ExampleGame/Assets/HockeyAppUnityAndroid/HockeyAppUnity-Scripts/HockeyAppAndroid.cs
@@ -62,6 +62,7 @@ public class HockeyAppAndroid : MonoBehaviour
 		if(exceptionLogging == true  && IsConnected() == true) {
 			List<string> logFileDirs = GetLogFiles();
 			if(logFileDirs.Count > 0) {
+				Debug.Log("Found files: " + logFileDirs.Count);
 				StartCoroutine(SendLogs(logFileDirs));
 			}
 		}
@@ -269,8 +270,6 @@ public class HockeyAppAndroid : MonoBehaviour
 		{
 			var versionCode = pluginClass.CallStatic<string>("getVersionCode");
 			var versionName = pluginClass.CallStatic<string>("getVersionName");
-			var sdkVersion = pluginClass.CallStatic<string>("getSdkVersion");
-			var sdkName = pluginClass.CallStatic<string>("getSdkName");
 			var manufacturer = pluginClass.CallStatic<string>("getManufacturer");
 			var model = pluginClass.CallStatic<string>("getModel");
 			var deviceIdentifier = pluginClass.CallStatic<string>("getDeviceIdentifier");
@@ -434,6 +433,9 @@ public class HockeyAppAndroid : MonoBehaviour
 					if (Debug.isDebugBuild)
 						Debug.Log ("Failed to delete exception log: " + e);
 				}
+			} else {
+				if (Debug.isDebugBuild)
+					Debug.Log ("Crash sending error: " + www.error);
 			}
 		}
 	}
@@ -519,7 +521,14 @@ public class HockeyAppAndroid : MonoBehaviour
 	{
 
 		#if (UNITY_ANDROID && !UNITY_EDITOR)
-		WriteLogToDisk(logString, stackTrace);
+		try
+		{
+			WriteLogToDisk(logString, stackTrace);
+		}
+		catch (Exception e)
+		{
+			AndroidLog(e.ToString());
+		}
 		#endif
 	}
 
@@ -575,6 +584,12 @@ public class HockeyAppAndroid : MonoBehaviour
 				}));
 		}
 		return javaMap;
+	}
+
+	private static void AndroidLog(string message)
+	{
+		var logClass = new AndroidJavaObject("android.util.Log");
+		logClass.CallStatic<int>("d", "HockeyApp", message);
 	}
 
 	#endif
